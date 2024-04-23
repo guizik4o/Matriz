1d771754ccaa83fbb03e1ebcd130083635a597f1
guizik4o <guilhermezexal@gmail.com>
2024-04-22 23:57:20 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#=== LISATAS MULTIDIMENCIONAIS ===
+matriz = [[87, 28, 32, 1], 
+          [2, 27, 520, 10], 
+          [5, 17, 87, 64]]
+elemento = matriz[2]
+
+minha_lista = [2, 25, 6, 37]
+print (minha_lista[0])
+print (elemento)
+print (matriz[2][1])
